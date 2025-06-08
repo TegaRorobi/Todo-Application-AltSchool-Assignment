175d3bfc94e46d1e475f74c70e22ce1436e19e93
Tega Rorobi <rorobitega@gmail.com>
2025-06-08 23:35:23 +0100

    feat: add models/User.js User Mongoose schema definition

---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    passwordHash: { // Stores the hashed password
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+module.exports = mongoose.model('User', UserSchema);
